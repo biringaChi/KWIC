7410214d210a9a9a917ef182ff4cbaeeacd41f50
GradXL49 <gradxl1573@gmail.com>
2022-01-31 19:00:32 -0500

    Added Input and Output modules. Created a directory for documentation and included our initial class diagram.

---
+++ output.py
@@ -0,0 +1,9 @@
+# Grady Landers
+# KWIC Implementation 1 - Output module
+
+# output(List<String>): void
+# In this implementation, output() prints a number of strings to the console
+def output(lines):
+    print("Results:")
+    for line in lines:
+        print(line)
