f683bcfa1ff54ecbd97510103dff9f25c3aa3877
GradXL49 <gradxl1573@gmail.com>
2022-02-01 16:18:06 -0500

    Made input and output to be object-oriented.

--- input.py
+++ input.py
@@ -1,18 +1,21 @@
 # Grady Landers
 # KWIC Implementation 1 - Input module
 
-# getData(): List<String>
-# In this implementation, getData() collects a number of strings from the keyboard to be returned to the method caller
-def getData():
-    lines = []
-    print("Enter lines to be processed by the KWIC program. Enter an empty line to finish:")
+class Input:
+    # getData(): List<String>
+    # In this implementation, getData() collects a number of strings from the keyboard
+    # to be returned to the method caller
+    @staticmethod
+    def getData():
+        lines = []
+        print("Enter lines to be processed by the KWIC program. Enter an empty line to finish:")
 
-    collecting = True
-    while collecting:
-        s = input()
-        if s == "":
-            collecting = False
-        else:
-            lines.append(s)
+        collecting = True
+        while collecting:
+            s = input()
+            if s == "":
+                collecting = False
+            else:
+                lines.append(s)
 
-    return lines
+        return lines
--- output.py
+++ output.py
@@ -1,9 +1,11 @@
 # Grady Landers
 # KWIC Implementation 1 - Output module
 
-# output(List<String>): void
-# In this implementation, output() prints a number of strings to the console
-def output(lines):
-    print("Results:")
-    for line in lines:
-        print(line)
+class Output:
+    # output(List<String>): void
+    # In this implementation, output() prints a number of strings to the console
+    @staticmethod
+    def output(lines):
+        print("Results:")
+        for line in lines:
+            print(line)
