ab7d261a58dc365134cd93eba25d925f3cebcfaf
sarthaksanyasi <34741404+sarthaksanyasi@users.noreply.github.com>
2022-02-04 12:23:17 -0500

    Add files via upload

---
+++ Implementation1/alphabetizer.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+#author: Sarthak Sanyasi
+#usage: Alphabetizer function
+
+from typing import List
+
+class Alphabetizer:
+    def __init__(self):
+        pass
+
+    def alphabetizer(self, output_list: List[str]):
+        return output_list.sort()
