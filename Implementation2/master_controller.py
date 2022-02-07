31ff2bea678071fbc0ebbbc5f5b6848b41a14010
Moul_10 <alexander.moulton@outlook.com>
2022-02-07 10:12:42 -0500

    fixed input, output, line_storage, added input_file.txt

--- Implementation2/master_controller.py
+++ Implementation2/master_controller.py
@@ -1,4 +1,4 @@
-'''
+"""
 
 Author:
     Alexander J. Moulton
@@ -7,7 +7,7 @@ Author:
 Usage:
     just used to run the modules in sequence
 
-'''
+"""
 
 from line_storage import LineStorage
 from input import Input
@@ -17,12 +17,20 @@ from output import Output
 
 
 def main():
+    """
+    Object Constructions
+    """
+
     storage = LineStorage()
     input = Input()
     shifter = CircularShifter()
     alphabetizer = Alphabetizer()
     output = Output()
 
+    '''
+    Process:
+    get input, store raw lines, shift raw lines, alphabetize shifted lines, output to file
+    '''
     lines = input.get_data()
 
     storage.store_lines(lines)
@@ -30,7 +38,7 @@ def main():
     permutations = []
     for line in lines:
         permutations.append(shifter.shift(line))
-
+    
     storage.store_permutations(permutations)
 
     output.output(alphabetizer.alphabetize(storage.get_permutations()))
--- Implementation2/output.py
+++ Implementation2/output.py
@@ -1,10 +1,24 @@
 #Author: Sarthak Sanyasi
 #implementation2 - output
 
+import os
 from typing import List
 
+
 class Output:
-    def output(self):
-        with open('input_file') as file:
-            for lines in file:
-                print(lines.strip())
+
+    def __init__(self):
+        pass
+
+    def output(self, lines: List[str]):
+        dir_name = os.path.dirname(__file__)
+        file_name = os.path.join(dir_name, 'output_file.txt')
+
+        # if output file already exists, remove it
+        if os.path.exists(file_name):
+            os.remove(file_name)
+        # once removed, recreate it and append each line to it
+        with open('output_file.txt', 'a+') as f:
+            for line in lines:
+                f.write(line + '\n')
+        f.close()
